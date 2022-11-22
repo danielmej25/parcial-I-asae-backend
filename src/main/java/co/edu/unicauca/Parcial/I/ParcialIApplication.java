5f79b86927852e0bf1e2bcb7287135fdca1af6ef
Daniel Mejía <daniel.mejia@segurosbolivar.com>
2022-11-22 17:42:43 -0500

    spring intializer project :rocket:

---
+++ src/main/java/co/edu/unicauca/Parcial/I/ParcialIApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.Parcial.I;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/Parcial/I/ParcialIApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.Parcial.I;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
