27d85343b76af9d9fc491de80c610db9607aa4c5
Daniel Mejía <daniel.mejia@segurosbolivar.com>
2022-11-22 19:00:10 -0500

    configuracion build.yml de github actions

--- src/test/java/co/edu/unicauca/Parcial/I/ParcialIApplicationTests.java
+++ src/test/java/co/edu/unicauca/Parcial/I/ParcialIApplicationTests.java
@@ -1,13 +1,28 @@
 package co.edu.unicauca.Parcial.I;
 
+import co.edu.unicauca.Parcial.I.controller.DataController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ParcialIApplicationTests {
 
+	@Autowired
+	DataController dataController;
+
 	@Test
-	void contextLoads() {
+	void health() {
+		assertEquals( "HEALTH CHECK OK!", dataController.healthCheck());
+	}
+
+	@Test
+	void version() {
+		assertEquals( "The actual version is 1.0.0", dataController.version());
 	}
 
 }
+
+
