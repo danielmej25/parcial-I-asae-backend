b32928a730e5244e46e0e0dd93a120a7f0334127
Daniel Mejía <daniel.mejia@segurosbolivar.com>
2022-11-25 19:15:37 -0500

    rename paquete y agregar modelmapper

--- src/main/java/co/edu/unicauca/parcial/ParcialIApplication.java
+++ src/main/java/co/edu/unicauca/parcial/ParcialIApplication.java
@@ -1,4 +1,4 @@
-package co.edu.unicauca.Parcial.I;
+package co.edu.unicauca.parcial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
