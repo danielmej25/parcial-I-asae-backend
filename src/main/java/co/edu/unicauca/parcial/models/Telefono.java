374467cb12b4e04ecde8fd53117e24e549c75916
Daniel Mejía <daniel.mejia@segurosbolivar.com>
2022-11-27 01:17:05 -0500

    conexion DB y models :floppy_disk:

---
+++ src/main/java/co/edu/unicauca/parcial/models/Telefono.java
@@ -0,0 +1,31 @@
+package co.edu.unicauca.parcial.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "telefono")
+@Getter
+@Setter
+public class Telefono implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_telefono")
+    private int id;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @Column(nullable = false)
+    private String numero;
+
+    @ManyToOne
+    @JoinColumn(name = "id_persona")
+    private Estudiante estudiante;
+}
